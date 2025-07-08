830704678903d14d169b15b6eaaf104c2713bc2e
Yoo Seung-jun <funczun@gmail.com>
2025-07-08 23:18:03 +0900

    [Bronze II] Title: 방 배정, Time: 120 ms, Memory: 14208 KB, Score: 2 point -BaekjoonHub

---
+++ 백준/Bronze/13300. 방 배정/방 배정.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[][] map = new int[2][7];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int gen = Integer.parseInt(st.nextToken());
+            int gra = Integer.parseInt(st.nextToken());
+            map[gen][gra] += 1;
+        }
+
+        int rms = 0;
+
+        for (int i = 0; i < 7; i++) {
+            if (map[0][i] % 2 == 0) {
+                rms -= 1;
+            }
+            if (map[1][i] % 2 == 0) {
+                rms -= 1;
+            }
+            rms += map[0][i] / 2 + 1;
+            rms += map[1][i] / 2 + 1;
+        }
+
+        System.out.print(rms);
+    }
+}
