4ea9ead0c40b95b75a611b1d0512b9db3029a371
Yoo Seung-jun <funczun@gmail.com>
2025-08-05 17:47:11 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 188 ms, Memory: 18360 KB -BaekjoonHub

---
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        int e = sc.nextInt();
+        int f = sc.nextInt();
+
+        int x = (c * e - f * b) / (a * e - d * b);
+        int y = (c * d - f * a) / (b * d - e * a);
+
+        System.out.println(x + " " + y);
+    }
+}
