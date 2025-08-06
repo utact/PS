2c7d96d72b680d393775b591f5793574b2ab0203
Yoo Seung-jun <funczun@gmail.com>
2025-08-06 23:17:21 +0900

    [Silver I] Title: 숨바꼭질, Time: 136 ms, Memory: 16528 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[100001];
+
+        Queue<Integer> q = new ArrayDeque<>();
+        q.offer(N);
+
+        while (!q.isEmpty()) {
+            int cur = q.poll();
+
+            if (cur == K) {
+                System.out.print(arr[K]);
+                break;
+            }
+
+            if (cur - 1 >= 0 && arr[cur - 1] == 0) {
+                arr[cur - 1] = arr[cur] + 1;
+                q.offer(cur - 1);
+            }
+
+            if (cur + 1 <= 100000 && arr[cur + 1] == 0) {
+                arr[cur + 1] = arr[cur] + 1;
+                q.offer(cur + 1);
+            }
+
+            if (cur * 2 <= 100000 && arr[cur * 2] == 0) {
+                arr[cur * 2] = arr[cur] + 1;
+                q.offer(cur * 2);
+            }
+        }
+    }
+}
