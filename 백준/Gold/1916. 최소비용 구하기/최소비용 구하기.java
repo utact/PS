a8d63e28b10c9ad2df614a17a668b242df542ec5
Yoo Seung-jun <funczun@gmail.com>
2025-09-08 09:36:42 +0900

    [Gold V] Title: 최소비용 구하기, Time: 460 ms, Memory: 51160 KB -BaekjoonHub

---
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -0,0 +1,89 @@
+import java.io.*;
+import java.util.*;
+
+/*
+ * 도시 수 N 1,000
+ * 버스 수 M 100,000
+ * 
+ * 출발 -> 도착 -> 비용 100,000
+ */
+
+public class Main {
+	static int N, M;
+	static int[] dist;
+	static PriorityQueue<Node> pq;
+	static ArrayList<ArrayList<Node>> graph;
+	
+	static class Node implements Comparable<Node> {
+		int from;
+		int cost;
+		
+		public Node(int from, int cost) {
+			super();
+			this.from = from;
+			this.cost = cost;
+		}
+
+		@Override
+		public int compareTo(Node o) {
+			return this.cost - o.cost;
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		// ㅊㄱㅎ
+		N = Integer.parseInt(br.readLine());
+		M = Integer.parseInt(br.readLine());
+		
+		dist = new int[N + 1];
+		Arrays.fill(dist, Integer.MAX_VALUE);
+		
+		pq = new PriorityQueue<>();
+		
+		graph = new ArrayList<>();
+		for (int i = 0; i < N + 1; i++) {
+			graph.add(new ArrayList<>());
+		}
+		
+		// ㅇㅈㄹㅅㅌ
+		for (int i = 0; i < M; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int from = Integer.parseInt(st.nextToken());
+			int to = Integer.parseInt(st.nextToken());
+			int cost = Integer.parseInt(st.nextToken());
+			graph.get(from).add(new Node(to, cost));
+		}
+		
+		// ㅅㅈㅈ, ㄲㅈ
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int tgf = Integer.parseInt(st.nextToken());
+		int tgt = Integer.parseInt(st.nextToken());
+		
+		// ㄷㅇㅅㅌㄹ
+		pq.add(new Node(tgf, 0));
+		dist[tgf] = 0;
+		
+		while (!pq.isEmpty()) {
+			Node cur = pq.poll();
+			int from = cur.from;
+			int cost = cur.cost;
+			
+			if (cost > dist[from]) continue;
+
+			for (int i = 0; i < graph.get(from).size(); i++) {
+				
+				Node nn = graph.get(from).get(i);
+				
+				if (cost + nn.cost < dist[nn.from]) {
+					pq.add(new Node(nn.from, cost + nn.cost));
+					dist[nn.from] = cost + nn.cost;
+				}
+			}
+		}
+		
+		// ㅊㄹ
+		System.out.println(dist[tgt]);
+	}
+}
