ebb1923e2d59d673b07dbcba06463167273f6238
Yoo Seung-jun <funczun@gmail.com>
2025-09-12 14:53:20 +0900

    [Gold IV] Title: N-Queen, Time: 2500 ms, Memory: 14856 KB -BaekjoonHub

---
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -0,0 +1,47 @@
+import java.io.*;
+
+/*
+ * N-Queen
+ */
+
+public class Main {
+	static int N, ans;
+	static boolean[] col;
+	static boolean[] daig1;
+	static boolean[] daig2;
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		N = Integer.parseInt(br.readLine());
+		ans = 0;
+		col = new boolean[N];
+		daig1 = new boolean[N * 2 - 1];
+		daig2 = new boolean[N * 2 - 1];
+		
+		solve(0);
+		System.out.println(ans);
+	}
+	
+	static void solve(int r) {
+		if (r == N) {
+			ans++;
+			return;
+		}
+		
+		for (int c = 0; c < N; c++) {
+			// (N - 1)은 대각선 인덱스 보정
+			if (!col[c] && !daig1[r + c] && !daig2[r - c + (N - 1)]) {
+				col[c] = true;
+				daig1[r + c] = true;
+				daig2[r - c + (N - 1)] = true;
+				
+				solve(r + 1);
+				
+				col[c] = false;
+				daig1[r + c] = false;
+				daig2[r - c + (N - 1)] = false;
+			}
+		}
+	}
+}
