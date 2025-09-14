ccd9d0aa8bf3097265a2fe2736a8fd2ae4dfd1bf
Yoo Seung-jun <funczun@gmail.com>
2025-09-14 13:46:46 +0900

    [Silver III] Title: N과 M (4), Time: 136 ms, Memory: 17260 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, M;
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+
+        dfs(1, 0);
+        System.out.print(sb);
+    }
+
+    static void dfs(int stt, int depth) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(arr[i]).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for (int i = stt; i <= N; i++) {
+            arr[depth] = i;
+            dfs(i, depth + 1);
+        }
+    }
+}
