6d6b81383c5a693f2a6ec28cc3119da73239e728
Yoo Seung-jun <funczun@gmail.com>
2025-09-15 17:30:13 +0900

    [Gold III] Title: 가장 오래 걸리는 스도쿠, Time: 92 ms, Memory: 13920 KB, Score: 136 point -BaekjoonHub

---
+++ 백준/Gold/12095. 가장 오래 걸리는 스도쿠/가장 오래 걸리는 스도쿠.java
@@ -0,0 +1,13 @@
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("0 0 0 0 0 0 0 9 8\n"
+				+ "0 0 0 0 0 7 6 0 0\n"
+				+ "0 0 0 0 5 0 0 0 0\n"
+				+ "0 0 0 4 0 0 0 0 0\n"
+				+ "0 0 3 0 0 0 0 0 0\n"
+				+ "0 2 0 0 0 0 0 0 0\n"
+				+ "1 0 0 0 0 0 0 0 0\n"
+				+ "0 0 0 0 0 0 0 0 0\n"
+				+ "0 0 0 0 0 0 0 0 0");
+	}
+}
