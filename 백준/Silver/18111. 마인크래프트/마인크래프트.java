752bd14b6704f645df53a43d8c7cc1204d3b9e8c
Yoo Seung-jun <funczun@gmail.com>
2025-09-18 15:37:35 +0900

    [Silver II] Title: 마인크래프트, Time: 348 ms, Memory: 34160 KB -BaekjoonHub

--- 백준/Silver/18111. 마인크래프트/마인크래프트.java
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.java
@@ -2,24 +2,14 @@ import java.io.*;
 import java.util.*;
 
 /*
- * 블록 수거하기 -> 2초
- * 블록 설치하기 -> 1초
- * 
- * 시작 시 B개의 블록 가진 채 시작
- * >> 최소 시간, 땅의 높이 구하기
- * 
- * 1. 최초 범위 (0 <= h <= 256)
- * 2. 탐색 범위 줄이기 (최소 값 <= h <= 입력된 전체 블록 + 인벤토리 블록)
+ * 2차원 맵 -> 1차원 높이 배열
  */
 
 public class Main {
 	static int N, M, B;
-	static int[][] map;
+	static int[] block = new int[257];
 
-	// 스코프 범위
 	static int stt, end;
-	
-	// 정답
 	static int ansT, ansH;
 	
 	public static void main(String[] args) throws Exception {
@@ -30,24 +20,22 @@ public class Main {
 		M = Integer.parseInt(st.nextToken());
 		B = Integer.parseInt(st.nextToken());
 		
-		map = new int[N][M];
-		
-		int sum = 0;
-		int min = 256;
+		int minH = 256;
+		int maxH = 0;
 		
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < M; j++) {
 				int v = Integer.parseInt(st.nextToken());
-				map[i][j] = v;
+				block[v]++;
 				
-				sum += v;
-				min = Math.min(min, v);
+				minH = Math.min(minH, v);
+				maxH = Math.max(maxH, v);
 			}
 		}
 		
-		stt = (sum + B) / (N * M);
-		end = min;
+		stt = maxH;
+		end = minH;
 		
 		ansT = Integer.MAX_VALUE;
 		ansH = -1;
@@ -61,26 +49,24 @@ public class Main {
 	
 	static void getAns(int h) {
 		int t = 0;
-
-		int add = B;
-		int rmv = 0;
+		int addAvAmt = B;
 		
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				int cur = map[i][j];
-				
-				if (cur > h) {
-					add += (cur - h);
-					t += (cur - h) * 2;
-				}
-				else if (cur < h) {
-					rmv += (h - cur);
-					t += (h - cur) * 1;
-				}
+		for (int cur = 0; cur <= 256; cur++) {
+			if (block[cur] == 0) continue;
+			
+			if (cur > h) {
+				int diff = cur - h;
+				t += (diff * 2) * block[cur];
+				addAvAmt += diff * block[cur];
+			}
+			else if (cur < h) {
+				int diff = h - cur;
+				t += (diff * 1) * block[cur];
+				addAvAmt -= diff * block[cur];
 			}
 		}
 		
-		if (rmv > add || t >= ansT) return;
+		if (addAvAmt < 0 || t >= ansT) return;
 		
 		ansT = t;
 		ansH = h;
