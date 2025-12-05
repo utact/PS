aa1334a02b24a8ba967b626f619cbc0e8e445c48
Yoo Seung-jun <funczun@gmail.com>
2025-12-05 13:58:19 +0900

    [Bronze V] Title: A+B, Time: 100 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -6,7 +6,10 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
 
-        System.out.println(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()));
+        System.out.println(A + B);
     }
 }
